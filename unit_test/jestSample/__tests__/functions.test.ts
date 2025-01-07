9e0fb91e477090026461462f28e58e470bbcd381
tom106gunma <tom106.net@gmail.com>
2025-01-08 07:27:27 +0900

    課題2-1の解答を作成

--- unit_test/jestSample/__tests__/functions.test.ts
+++ unit_test/jestSample/__tests__/functions.test.ts
@@ -1,1 +1,33 @@
+import { sumOfArray, asyncSumOfArray} from "../functions";
+
 // todo: ここに単体テストを書いてみましょう！
+describe("sumOfArrayのテスト", () => {
+  test("[1,1]を渡したら2になること", () => {
+    expect(sumOfArray([1,1])).toBe(2);
+  });
+
+  test("[]を渡したら例外が発生すること", () => {
+    expect(() => sumOfArray([])).toThrow();
+  });
+  // 無名関数でラップする。toThrowが呼ばれる前にエラーで停止してしまうから。
+
+  test("マイナスが含まれていても正しく合計されること", () => {
+    expect(sumOfArray([-1, -2, 3])).toBe(0);
+  });
+
+  test("1つの要素しかない場合、その値が返ること", () => {
+    expect(sumOfArray([14])).toBe(14);
+  });
+
+});
+
+describe("asyncSumOfArrayのテスト", () => {
+  test("[1,1]を渡したら2になること", async () => {
+    expect(await asyncSumOfArray([1,1])).toBe(2);
+  });
+
+  test("[]を渡したら例外が発生すること", async () => {
+    await expect(() => asyncSumOfArray([])).rejects.toThrow();
+  });
+
+});
