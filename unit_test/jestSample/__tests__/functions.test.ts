b6f58140a7f0197de344c52a30e7652ba7dc10d4
tom106gunma <tom106.net@gmail.com>
2025-01-11 23:39:21 +0900

    課題2-2の解答を作成

--- unit_test/jestSample/__tests__/functions.test.ts
+++ unit_test/jestSample/__tests__/functions.test.ts
@@ -1,4 +1,5 @@
-import { sumOfArray, asyncSumOfArray} from "../functions";
+import { sumOfArray, asyncSumOfArray, asyncSumOfArraySometimesZero, getFirstNameThrowIfLong} from "../functions";
+import { NameApiService } from "../nameApiService";
 
 // todo: ここに単体テストを書いてみましょう！
 describe("sumOfArrayのテスト", () => {
@@ -30,4 +31,45 @@ describe("asyncSumOfArrayのテスト", () => {
     await expect(() => asyncSumOfArray([])).rejects.toThrow();
   });
 
+});
+
+describe("asyncSumOfArraySometimesZeroのテスト", () => {
+  // save をモック化
+  const mockDatabase = {
+    save: jest.fn()
+  };
+  test("[1,1]を渡したら2になること", async () => {
+    const numbers = [1, 1];
+    expect(await asyncSumOfArraySometimesZero(numbers, mockDatabase)).toBe(2);
+  });
+
+  test("[]を渡したらエラーとなり、0になること", async () => {
+    expect(await asyncSumOfArraySometimesZero([], mockDatabase)).toBe(0);
+  });
+
+  test("saveが失敗したら、0になること", async () => {
+    // save が失敗するようにモック化
+    const mockErrorDatabase = {
+      save: jest.fn(() => {
+        throw new Error("fail!");
+      })
+    };
+    expect(await asyncSumOfArraySometimesZero([], mockErrorDatabase)).toBe(0);
+  });
+});
+
+jest.mock("../nameApiService");
+describe("getFirstNameThrowIfLongのテスト", () => {
+  const mockNameApiService = new NameApiService() as jest.Mocked<NameApiService>;
+  mockNameApiService.getFirstName.mockResolvedValue("tom");
+
+  test("名前が返されること", async () => {
+    expect(await getFirstNameThrowIfLong(4, mockNameApiService)).toBe("tom");
+  });
+
+  test("maxNameLength を超えた場合、エラーが返される", async () => {
+    await expect(getFirstNameThrowIfLong(2, mockNameApiService)).rejects.toThrow(
+      "first_name too long"
+    );
+  });
 });
