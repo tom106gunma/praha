0c755ae51204ad73fb5196b0c7faccd676093074
tom106gunma <tom106.net@gmail.com>
2025-01-20 23:40:24 +0900

    Play function機能を使って、Gameコンポーネントのstoryにインタラクションのテストを追加

--- storybook/react_tutorial/src/components/Game.stories.js
+++ storybook/react_tutorial/src/components/Game.stories.js
@@ -1,8 +1,29 @@
+import React from 'react';
+import { userEvent, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
 import Game from './Game';
 
 export default {
   title: 'Components/Game',
   component: Game,
-};
+} ;
+
+const Template = (args) => <Game {...args} />;
 
 export const Default = () => <Game />;
+
+export const WinnerX = Template.bind({});
+WinnerX.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  const squareButtons = canvas.getAllByRole('button');
+
+  await userEvent.click(squareButtons[0]);
+  await userEvent.click(squareButtons[3]);
+  await userEvent.click(squareButtons[1]);
+  await userEvent.click(squareButtons[4]);
+  await userEvent.click(squareButtons[2]);
+
+  expect(canvas.getByText("Winner: X")).toBeInTheDocument();
+};
